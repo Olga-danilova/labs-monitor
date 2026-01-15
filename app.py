d5dee35a8e61c9507c05ea398d17aa1ffa80d2ca
Olga-danilova <124255405+Olga-danilova@users.noreply.github.com>
2026-01-15 11:32:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -50,5 +50,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    port = int(os.getenv('PORT') or 8080)
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080)
