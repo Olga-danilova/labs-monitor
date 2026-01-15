6978822c67b63e69e67549d3119b01acf5f78caa
Olga-danilova <124255405+Olga-danilova@users.noreply.github.com>
2026-01-15 11:23:16 +0300

    Fix port in app.py: change from 8000 to 8080

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    port = int(os.getenv('PORT') or 8000)
+    port = int(os.getenv('PORT') or 8080)
     app.run(debug=True, host='0.0.0.0', port=port)
