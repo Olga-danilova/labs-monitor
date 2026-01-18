a3cd1734fec50b50d68d52f2172c24710b5525dc
Olga-danilova <124255405+Olga-danilova@users.noreply.github.com>
2026-01-18 23:08:53 +0300

    Fix syntax error - restore line 1 import statement

--- routes.py
+++ routes.py
@@ -1,5 +1,4 @@
-:31
- flask import Blueprint, render_template, redirect, url_for, request, jsonify, make_response
+from flask import Blueprint, render_template, redirect, url_for, request, jsonify, make_response
 from flask_login import login_user, login_required, logout_user, current_user
 from database import db
 from models import User, Group, Student, Lesson, Grade, Plan, Announcement
