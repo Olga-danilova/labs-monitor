6c6a0c541ae7cea0f680e6f4bf7651cc9170e5ba
Olga-danilova <124255405+Olga-danilova@users.noreply.github.com>
2026-01-19 22:00:04 +0300

    Fix: Remove int() conversion from load_user

--- app.py
+++ app.py
@@ -30,8 +30,7 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))
-
+        return User.query.get(user_id)
     from routes import main_bp
     app.register_blueprint(main_bp)
 
