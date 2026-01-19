33af752f5d6fe2d5a83d1ee1696382cb6e16903a
Olga-danilova <124255405+Olga-danilova@users.noreply.github.com>
2026-01-19 20:43:35 +0300

    Fix indentation error on line 61 in login function

--- routes.py
+++ routes.py
@@ -58,7 +58,7 @@ def login():
     if request.method == 'POST':
         user = User.query.get(request.form.get('id'))
         if user and verify_password(user.password, request.form.get('password')):            61
-                        login_user(user)
+                                    login_user(user)
            return redirect(url_for('main.index'))
     else:
             return render_app("""
